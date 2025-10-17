92573b5b2958a3db83480082645bcdf3a99a4dd1
matitab <matitab013@gmail.com>
2025-10-17 00:12:04 -0300

    implementación de GBFS

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,27 +3,28 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
+def manhattan(state: tuple[int, int], objetivo: tuple[int, int]) -> int:
+    return abs(state[0] - objetivo[0]) + abs(state[1] - objetivo[1])
 
 class GreedyBestFirstSearch:
     @staticmethod
-    def search(grid: Grid) -> Solution:
-        """Find path between two points in a grid using Greedy Best First Search
-
-        Args:
-            grid (Grid): Grid of points
-
-        Returns:
-            Solution: Solution found
-        """
-        # Initialize root node
+    def search(grid: Grid, h=manhattan) -> Solution:
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
-
-        # Initialize reached with the initial state
-        reached = {}
-        reached[root.state] = root.cost
-
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
-
+        if grid.objective_test(root.state):
+            return Solution(root, reached={root.state})
+        frontera = PriorityQueueFrontier()
+        frontera.add(root, h(root.state, grid.end))
+        reached = {root.state}
+        while not frontera.is_empty():
+            nodo_nuevo = frontera.pop()
+            for casilla in grid.actions(nodo_nuevo.state):
+                estado_sucesor = grid.result(nodo_nuevo.state, casilla)
+                if estado_sucesor in reached:
+                    continue
+                reached.add(estado_sucesor)
+                nuevo_costo = nodo_nuevo.cost + grid.individual_cost(nodo_nuevo.state, casilla)
+                nodo_sucesor = Node("", state=estado_sucesor, cost=nuevo_costo, parent=nodo_nuevo, action=casilla)
+                if grid.objective_test(estado_sucesor):
+                    return Solution(nodo_sucesor, reached)
+                frontera.add(nodo_sucesor, h(estado_sucesor, grid.end))
         return NoSolution(reached)
