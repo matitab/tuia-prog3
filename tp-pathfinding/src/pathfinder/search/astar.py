4051f8d3dcaf8960325c866e99f205048b1190a7
matitab <matitab013@gmail.com>
2025-10-17 00:03:16 -0300

    implementación de A*

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -3,27 +3,29 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
+def manhattan(node: Node, estado_objetivo: tuple) -> int:
+    return abs(node.state[0] - estado_objetivo[0]) + abs(node.state[1] - estado_objetivo[1])
 
 class AStarSearch:
     @staticmethod
-    def search(grid: Grid) -> Solution:
-        """Find path between two points in a grid using A* Search
-
-        Args:
-            grid (Grid): Grid of points
-
-        Returns:
-            Solution: Solution found
-        """
-        # Initialize root node
+    def search(grid: Grid, h=None) -> Solution:
+        if h is None:
+            h = lambda node: manhattan(node, grid.end)
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
-
-        # Initialize reached with the initial state
+        frontera = PriorityQueueFrontier()
+        frontera.add(root, root.cost + h(root))
         reached = {}
         reached[root.state] = root.cost
-
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
-
-        return NoSolution(reached)
+        while True:
+            if frontera.is_empty():
+                return NoSolution(reached)
+            n = frontera.pop()
+            if grid.objective_test(n.state):
+                return Solution(n, reached)
+            for a in grid.actions(n.state): # por cada acción
+                s = grid.result(n.state, a) # casilla resultante
+                c = n.cost + grid.individual_cost(n.state, a)
+                if s not in reached or c < reached[s]:
+                    nodo_sucesor = Node("", state=s, parent=n, action=a, cost=c)
+                    reached[s] = c
+                    frontera.add(nodo_sucesor, nodo_sucesor.cost + h(nodo_sucesor))
